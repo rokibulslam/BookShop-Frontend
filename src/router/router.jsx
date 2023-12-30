4bdbcc56f4808d8a7b443ae81b502ac4364f83b5
rokibulslam <rokibulslam6@gmail.com>
2023-12-30 09:40:31 +0600

    responsive navbar

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
 
 
 const router = createBrowserRouter([
-    {}
+    {
+        path: "/",
+        element: 
+            <Layout />
+        
+    }
 ])
 
 export default router;
